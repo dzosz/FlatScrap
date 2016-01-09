d11599d7e7f088a68af01ca0025d0283c637e9bd
dzosz <tomek3319@gmail.com>
2016-01-09 10:16:34 +0100

    Add groups to config

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
+# FLASK APP
 DEBUG = False
 PROPAGATE_EXCEPTIONS = True
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
+
+# DATABASE
 HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS','localhost')
 APP_NAME = os.environ.get('OPENSHIFT_APP_NAME','flatscrap')
 IP = os.environ.get('OPENSHIFT_PYTHON_IP','127.0.0.1')
@@ -10,4 +13,6 @@ PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT',8080))
 REDIS_HOST = os.environ.get('OPENSHIFT_REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('OPENSHIFT_REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
+
+# WEBSCRAPER
 MAX_PAGE_NUMBER = 10
