a43b77fa234d078a0e5d5b5af17ea95d59cb9fbf
dzosz <tomek3319@gmail.com>
2016-01-12 15:43:09 +0100

    fix mock in utilities test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,9 @@
-import requests
-import unittest
-from unittest import mock
+from unittest import TestCase, mock
 from utils import match_words
 from search import scrap_ad_list, scrap_subpage
 
 
-class UtilsTestCase(unittest.TestCase):
+class UtilsTestCase(TestCase):
 
     def test_street_regex(self):
         text="""mieszkanie na ulicy Reja 83 , blisko do Pasaży \
@@ -13,7 +11,7 @@ class UtilsTestCase(unittest.TestCase):
         dobre połączenie z Rynkiem , dworcem pkp i pks (ul.piastowska, \
         górnickiego -10 min) pokój duży słoneczny z wyjsciem na mały \
         ogródek , obecnie mieszkają w nim dwie dziewczyny od stycznia \
-        będzie wolny dla jednej osoby , w mieszkaniu wyposażona kuchnia, \
+        będzie wolny dla jednej osoby , w mieszkaniu wyposażona kuchnia,\
         łazienka i pokój jednoosobowy wynajmowany przez chłopaka. \
         Współlokatorzy mili, spokojni, niekonfliktowi. Studentka i \
         student z UP. W czynsz 625 zł wliczone wszystkie opłaty, \
@@ -26,19 +24,16 @@ class UtilsTestCase(unittest.TestCase):
 
     @mock.patch('requests.get')
     def test_ad_finder(self, mock_get):
-        mock_response = mock.Mock()
-        with open('tests/mainpage_mock.html') as opener:
-            mock_response.content = opener.read()
-        mock_get.return_value = mock_response
+
+        with open('tests/mainpage.html') as opener:
+            mock_get.return_value.content = opener.read()
 
         self.assertEqual(len(scrap_ad_list('fakelink')), 2)
 
     @mock.patch('requests.get')
     def test_subpage_read(self, mock_get):
-        mock_response = mock.Mock()
-        with open('tests/subpage_mock.html') as opener:
-            mock_response.content = opener.read()
-        mock_get.return_value = mock_response
+        with open('tests/subpage.html') as opener:
+            mock_get.return_value.content = opener.read()
         data_goal = {
             'title': 'Pokój do wynajęcia w okolicy Obornickiej',
             'price': 650,
@@ -47,6 +42,7 @@ class UtilsTestCase(unittest.TestCase):
         result = scrap_subpage('fakelink')
 
         self.assertDictEqual(scrap_subpage('fakelink'), data_goal)
+
 
 if __name__ == '__main__':
     unittest.main()
