31300c798eeed2b3ac6dad7054e81f9d642bb531
dzosz <tomek3319@gmail.com>
2016-01-21 13:43:33 +0100

    bind wsgi to all addresses

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
 # WSGI
 HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS','localhost')
 APP_NAME = os.environ.get('OPENSHIFT_APP_NAME','flatscrap')
-IP = os.environ.get('OPENSHIFT_PYTHON_IP','127.0.0.1')
-PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT',8080))
+IP = os.environ.get('OPENSHIFT_PYTHON_IP','0.0.0.0')
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT',5000))
 
 # DATABASE
 REDIS_HOST = os.environ.get('OPENSHIFT_REDIS_HOST', '127.0.0.1')
